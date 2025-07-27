e46b2064787c20245be60271191184102266a7c3
Jack Waterloo <jack.waterloo77@gmail.com>
2025-07-27 12:37:22 -0500

    added CSS to package with JS

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 // index.js
+import "./styles.css"; //imports the css into js. reliant on "style-loader", "css-loader" in webpack
 import { greeting } from "./greeting.js";
 
 console.log(greeting);
